023c975e37a2b22f9635bfed668c0a18f4e4289a
Saana Kanniainen <naanatin@gmail.com>
2024-10-29 13:53:58 +0200

    US3 Test 2

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -65,3 +65,18 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(2, 6))
 
         self.assertEqual(88, game.calculate_score())
+
+    def test_count_game_score_with_two_spares(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 9))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 2))
+        game.add_frame(Frame(2, 6))
+
+        self.assertEqual(91, game.calculate_score())
