baf4921e72d10fa9df6e724097b3acd691035352
Saana Kanniainen <naanatin@gmail.com>
2024-10-29 12:56:23 +0200

    US1 Test 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -4,8 +4,20 @@ from bowling import BowlingGame
 from bowling_error import BowlingError
 from frame import Frame
 
-
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_create_game_10_frames(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        self.assertEqual([1, 5], game.get_frame_at(0))
+
