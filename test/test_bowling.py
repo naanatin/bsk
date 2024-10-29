aac5e84577975f55fd8531c86d3a7825fe45a588
Saana Kanniainen <naanatin@gmail.com>
2024-10-29 13:12:21 +0200

    US2 Test 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -36,3 +36,18 @@ class TestBowlingGame(unittest.TestCase):
 
         self.assertRaises(BowlingError, game.add_frame, Frame(1, 5))
 
+    def test_count_came_score(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        self.assertEqual(81, game.calculate_score())
+
