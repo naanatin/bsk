53fb7e5bd72bca9ae14d6597e2b043915ebd738f
Saana Kanniainen <naanatin@gmail.com>
2024-10-29 13:06:33 +0200

    US1 Test 2

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -21,3 +21,18 @@ class TestBowlingGame(unittest.TestCase):
 
         self.assertIsNotNone(game.get_frame_at(9))
 
+    def test_11_frames_should_raise_bowling_error(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        self.assertRaises(BowlingError, game.add_frame, Frame(1, 5))
+
