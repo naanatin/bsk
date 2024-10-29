c95a92079503f48cb1cd0aeaca10d8a4b16551c7
Saana Kanniainen <naanatin@gmail.com>
2024-10-29 13:03:32 +0200

    US1 Create game with 10 frames

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -19,5 +19,5 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(8, 1))
         game.add_frame(Frame(2, 6))
 
-        self.assertEqual([1, 5], game.get_frame_at(0))
+        self.assertIsNotNone(game.get_frame_at(9))
 
